0f86a05a9eadf93e202907a859f16a07fd56ceec
Subhadeep Sasmal <subhadeepsasmal2@gmail.com>
2024-08-18 20:04:47 +0530

    Update api

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const WeatherBtn = document.querySelector("#getWeatherBtn");
 const input = document.querySelector("#cityInput");
 const div = document.querySelector("#weatherResult");
 
-const apikey = `44c0411164dc00b3e1077f4b615b8c3a`;
+const apikey = `2c468cb1a4d94ba2835134513241808`;
 
 WeatherBtn.addEventListener("click", async () => {
     const city = input.value;
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apikey}`;
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=yes`;
     input.value = '';
     if (city === '') {
         alert('Please enter a city name');
@@ -15,16 +15,13 @@ WeatherBtn.addEventListener("click", async () => {
     try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        if (data.cod === '404') {
-           div.innerHTML = `Error: ${data.error}`; 
-        }
-        else if (data.cod !== 200) {
-            div.innerHTML = `Error: ${data.message}`;
+        if (data.error) {
+           div.innerHTML = `Error: ${data.error.message}`; 
         }
         else {
-            const temperature = data.main.temp;
-            const description = data.weather[0].description;
-            div.innerHTML = `<p><strong>${city}</strong></p>
+            const temperature =data.current.temp_c;
+            const description = data.current.condition.text; 
+            div.innerHTML = `<p><strong>${city},${data.location.country}</strong></p>
                             <p>Temperature: ${temperature}°C </p>
                             <p>Condition: ${description}</p>`;
         }
